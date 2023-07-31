c5064e497ef7d5e2b920e64d1336bbd269c6a8da
Gloria Williams <willyabiodun@gmail.com>
2023-07-31 19:44:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const change = document.getElementById("powder");
+
+ change .addEventListener("click", (event) => {
+    event.target.style.background ="blue";
+    event.target.style.color = "white";
+ });
+
+ const change2 = document.getElementById("foundation");
+ change2 .addEventListener("click", (event) => {
+    event.target.style.background ="black";
+    event.target.style.color = "white";
+ });
+
+ const change3 = document.getElementById("primer");
+ change3 .addEventListener("click", (event) => {
+    event.target.style.background ="red";
+    event.target.style.color = "white";
+ });
+
+ const change4 = document.getElementById("highlighter");
+ change4 .addEventListener("click", (event) => {
+    event.target.style.background ="green";
+    event.target.style.color = "white";
+ });
+
+ const change5 = document.getElementById("eyeShadow");
+ change5 .addEventListener("click", (event) => {
+    event.target.style.background ="purple";
+    event.target.style.color = "white";
+ });
+
+ const change6 = document.getElementById("beautyBlender");
+ change6 .addEventListener("click", (event) => {
+    event.target.style.background ="brown";
+    event.target.style.color = "white";
+ });
+
+ const change7 = document.getElementById("concealer");
+ change7 .addEventListener("click", (event) => {
+    event.target.style.background ="rgb(85, 173, 3)";
+    event.target.style.color = "white";
+ });
+
+ const change8 = document.getElementById("spray");
+ change8 .addEventListener("click", (event) => {
+    event.target.style.background ="rgb(44, 14, 38)";
+    event.target.style.color = "white";
+ });
